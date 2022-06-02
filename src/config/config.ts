bd548ef8c9a6839e844b0990a89b0d6681a0a400
Sparkin <sdparkin@protonmail.com>
2022-06-01 20:33:12 -0500

    Added JWT_SECRET

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 export const config = {
     mongo: {
@@ -11,5 +12,8 @@ export const config = {
     },
     server: {
         port: SERVER_PORT
+    },
+    jwt: {
+        secret: JWT_SECRET
     }
 };
